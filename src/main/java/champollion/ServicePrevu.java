2ffaa45c2500a61073a52090716ab1485916d948
lisacapitaine <147838571+lisacapitaine@users.noreply.github.com>
2023-10-23 15:55:02 +0300

    Update ServicePrevu.java

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,58 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
+
+    private int volumeCM, volumeTD, volumeTP;
+    private UE ue;
+    private Enseignant enseignant;
+
+    public ServicePrevu (int CM, int TD, int TP,UE ue, Enseignant e){
+        this.volumeCM = CM;
+        this.volumeTD = TD;
+        this.volumeTP = TP;
+        this.ue = ue ;
+        this.enseignant = e;
+
+    }
+	
+    public UE getUe() {
+        return ue;
+    }
+
+    public int getCM() {
+        return volumeCM;
+    }
+
+    public int getTD() {
+        return volumeTD;
+    }
+
+    public int getTP() {
+        return volumeTP;
+    }
+
+    public Enseignant getEnseignant() {
+        return enseignant;
+    }
+
+    public void setUe(UE ue) {
+        this.ue = ue;
+    }
+
+    public void setEnseignant(Enseignant enseignant) {
+        this.enseignant = enseignant;
+    }
+
+    public void setVolumeCM(int volumeCM) {
+        this.volumeCM = volumeCM;
+    }
+
+    public void setVolumeTD(int volumeTD) {
+        this.volumeTD = volumeTD;
+    }
+
+    public void setVolumeTP(int volumeTP) {
+        this.volumeTP = volumeTP;
+    }
 
 }
