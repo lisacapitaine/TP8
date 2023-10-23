0d1bb25dbfbf7b3598b7f681b00bbe14019ad7e4
lisacapitaine <147838571+lisacapitaine@users.noreply.github.com>
2023-10-23 15:57:38 +0300

    Update ChampollionJUnitTest.java

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -2,39 +2,46 @@ package champollion;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
 
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
-		
+
+
 	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
 		java = new UE("Programmation en java");		
-	}
-	
+		java = new UE("Programmation en java");	
 
-	@Test
-	public void testNouvelEnseignantSansService() {
-		assertEquals(0, untel.heuresPrevues(),
-                        "Un nouvel enseignant doit avoir 0 heures prévues");
 	}
-	
-	@Test
-	public void testAjouteHeures() {
-                // 10h TD pour UML
-		untel.ajouteEnseignement(uml, 0, 10, 0);
 
-		assertEquals(10, untel.heuresPrevuesPourUE(uml),
-                        "L'enseignant doit maintenant avoir 10 heures prévues pour l'UE 'uml'");
+        @Test
+        public void testHeuresPrevues(){
+            untel.ajouteEnseignement(uml, 0, 1, 0);
+            untel.ajouteEnseignement(java, 0, 1, 0);
+            assertEquals(2, untel.heuresPrevues(),"L'enseignant doit réaliser 4h" );
+        }
+        
+       
+        @Test
+        public void testHeuresPlanifiees(){
+            Salle s = new Salle ("Salle 1", 25);
+            Intervention e = new Intervention (s, uml, untel, new Date(), 2, TypeIntervention.TD);
+            untel.Intervention(e);
+            assertEquals(2, untel.heuresPlanifiees(), "L'enseignant doit réaliser 2h");
+        }
 
-                // 20h TD pour UML
-                untel.ajouteEnseignement(uml, 0, 20, 0);
-                
-		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml),
-                         "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
-		
-	}
-	
+        @Test 
+        public void testEnSousService(){
+            untel.ajouteEnseignement(uml, 1, 1, 1);
+            assertEquals(true, untel.sousService(), "L'enseignant est en sous service");
+
+            untel.ajouteEnseignement(uml, 100, 50, 50);
+            assertEquals(true, untel.sousService(), "L'enseignant n'est pas en sous service");
+
+        }
+
 }
